b824032755fb75bdc41ab0991cd06f2d7cfa7b2d
Shemiah <shemiah@medlor.com>
2018-09-17 10:16:14 -0700

    Change backward slashes to forward

--- src/index.test.js
+++ src/index.test.js
@@ -10,7 +10,7 @@ describe('Our first test', () => {
 
 describe('index.html', () => {
   it('should say hello', (done) => {
-    const index = fs.readFileSync('.\\src\\index.html', "utf-8");
+    const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
       expect(h1.innerHTML).to.equal("Hello from index.html!");
