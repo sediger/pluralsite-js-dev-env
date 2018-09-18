25d926ccfa3091d4665eaf7fa83c907af579abff
Shemiah <shemiah@medlor.com>
2018-09-17 18:41:16 -0700

    Added surge

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id":1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+    {"id":2, "firstName":"Bill", "lastName":"Doe", "email":"bill@gmail.com"},
+    {"id":3, "firstName":"Jamal", "lastName":"Jamal", "email":"jamal@jamal.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
